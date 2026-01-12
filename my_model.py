c77d1f215f66664f9e0fb917d9ff66a76093f0a2
zfy10086 <70880745+zfy10086@users.noreply.github.com>
2026-01-12 11:10:06 +0800

    Update my_model.py

--- my_model.py
+++ my_model.py
@@ -19,3 +19,24 @@ class Resnet(nn.Module):
         self.resnet = self.my_model(x)
         x = self.fc(self.resnet)
         return x
+
+
+# ======Densenet
+class Densenet(nn.Module):
+    def __init__(self, pretrained:bool, local_tuning = False):
+        super(Densenet, self).__init__()
+        if pretrained:    
+            weights = models.DenseNet121_Weights.IMAGENET1K_V1
+            print("Loading weights version 'IMAGENET1K_V1'")
+        else:
+            weights = None
+        self.my_model = models.densenet121(weights = weights)
+
+        self.fc = nn.Linear(1000, 2)
+        nn.init.xavier_normal_(self.fc.weight.data)
+        self.fc.bias.data.zero_()
+
+    def forward(self, x):
+        self.resnet = self.my_model(x)
+        x = self.fc(self.resnet)
+        return x
