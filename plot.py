a23b829aa581d6bf5db25f28921d6201b13286ee
zfy10086 <70880745+zfy10086@users.noreply.github.com>
2026-05-04 20:50:19 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from typing import Union
 from tqdm import tqdm
@@ -8,10 +9,10 @@ import matplotlib.pyplot as plt
 def draw_genes(expr: pd.DataFrame,
                coor: pd.DataFrame,
                save_dir: str,
-               spots_size = 3,
+               spots_size = None,
                cmap = "inferno",
                alpha = 1.0,
-               marker: str = 'o',
+               marker: str = 's',
                facecolor: str = "white",
                edgecolor: str = "face",
                linewidth: Union[int, float] = 0,
@@ -49,6 +50,9 @@ def draw_genes(expr: pd.DataFrame,
     if isinstance(cmap, mpl.colors.ListedColormap):
         ax.set_prop_cycle(color=cmap.colors)
 
+    if spots_size is None:
+        spots_size = auto_spot_size(coor, ax)
+
     for i in tqdm(expr.columns):
         ax.scatter(coor.iloc[:, 0],
                    coor.iloc[:, 1],
@@ -76,3 +80,25 @@ def draw_genes(expr: pd.DataFrame,
 
     plt.close(fig)
 
+def auto_spot_size(coords, ax, scale=1.0):
+    """
+    coords: (n,2) spatial coordinates
+    ax: matplotlib axis
+    scale: scale factor of spots size
+    """
+    from sklearn.neighbors import NearestNeighbors
+    coords = np.asarray(coords)
+    nbrs = NearestNeighbors(n_neighbors=2).fit(coords)
+    distances, _ = nbrs.kneighbors(coords)
+    nn_dist = np.median(distances[:, 1])
+    x_min, x_max = coords[:, 0].min(), coords[:, 0].max()
+    y_min, y_max = coords[:, 1].min(), coords[:, 1].max()
+    fig = ax.figure
+    bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    width_inch, height_inch = bbox.width, bbox.height
+    x_scale = width_inch / (x_max - x_min)
+    y_scale = height_inch / (y_max - y_min)
+    scale_data_to_inch = min(x_scale, y_scale)
+    diameter_points = nn_dist * scale_data_to_inch * 72 * scale
+    s = diameter_points ** 2
+    return s
